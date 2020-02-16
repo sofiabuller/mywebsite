565d434da9372c3b3e97762ddb6124c35b46bb47
Sofia Poulsen <poulsen@MacBook-Air-2.local>
2020-02-16 19:38:44 +1100

    server commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile('index.html'));
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`))
